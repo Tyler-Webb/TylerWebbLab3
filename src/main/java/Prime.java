6d740674e123c1fd136d4e640902e2f1cc2e3bff
Tyler Webb <tgwebb@gus.pittstate.edu>
2019-09-28 10:51:02 -0500

    Finished isPrime

--- src/main/java/Prime.java
+++ src/main/java/Prime.java
@@ -1,3 +1,6 @@
+
+import java.util.Scanner;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -12,6 +15,27 @@ public class Prime {
     
     public static void main(String[] args) {
         
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("Enter a number to check if it's prime");
+        int chosenNumber = Integer.parseInt(keyboard.nextLine());
+        
+        if(isPrime(chosenNumber)){
+            System.out.println("Your number is prime!");
+        }else{
+            System.out.println("Your number is not prime");
+        }
+        
+    }
+    public static boolean isPrime(int testNum){
+        if(testNum <=1){
+            return false;
+        }
+        for(int i = 2; i < testNum; i++){
+            if(testNum % i == 0){
+                return false;
+            }
+        }
+        return true;
         
     }
 }
