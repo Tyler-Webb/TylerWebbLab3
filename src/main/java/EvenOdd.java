8ec5a8de3d6ede84f9b51f16bd130895598db028
Tyler Webb <tgwebb@gus.pittstate.edu>
2019-09-28 19:02:43 -0500

    Finished EvenOdd

--- src/main/java/EvenOdd.java
+++ src/main/java/EvenOdd.java
@@ -1,3 +1,6 @@
+
+import java.util.Random;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -11,7 +14,30 @@
 public class EvenOdd {
     
     public static void main(String[] args) {
+        Random random = new Random();
+        int evenCounter = 0;
+        int oddCounter = 0;
         
+        for(int i = 1; i <= 100; i++){
+            int ranNum = random.nextInt();
+
+            if(isEven(ranNum)){
+                evenCounter++;
+            }else{
+                oddCounter++;
+            }
+        }
+        System.out.println("Even count was: " + evenCounter);
+        System.out.println("Odd count was: " + oddCounter);
         
+    }
+    
+    public static boolean isEven(int testNum){
+        
+        if(testNum % 2 == 0){
+            return true;
+        }else{
+            return false;
+        }
     }
 }
